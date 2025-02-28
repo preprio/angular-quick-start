d9aa41e94607d1999bd944050b835e02140f2460
Mandy <88187770+mlingham79@users.noreply.github.com>
2025-02-28 10:21:33 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    PREPR_ENDPOINT: 'https://graphql.prepr.io/7f05e9e2f17f1b3d5e08dab3a565acbea8b87745473917e159f70ae1cf0334b9'
+    PREPR_ENDPOINT: '<YOUR-PREPR-GRAPHQL-URL>'
 };
